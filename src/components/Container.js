6401e27abb423eca43a83f75d0ba57ae38c5c137
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-03 22:32:49 -0500

    adds prop types

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Container({ children }) {
   return <div className="container mx-auto px-4">{children}</div>
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 }
