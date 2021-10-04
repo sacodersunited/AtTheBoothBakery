1218a6ddcfc555f0873538ec2252e9aae32d2ac6
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-03 22:28:55 -0500

    adds brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          pink: '#f24998',
+          darkPurple: '#3c2e59',
+          lightPurple: '#6d5e8c',
+          green: '#03a63c',
+          yellow: '#f29d35',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
