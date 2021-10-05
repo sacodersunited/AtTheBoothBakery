3eccbee7e5d152b590d3f58fa2438a93d99d5b19
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-05 18:21:45 -0500

    Continues homepage
    - adds Two Column Comp
    - adds Our Story section
    - starts form

---
+++ src/components/TwoCols.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TwoCols({ left, right }) {
+  return (
+    <div class="grid grid-cols-2 py-10 sm:max-w-screen-lg sm:mx-auto">
+      <div className="flex items-stretch mr-1">{left}</div>
+      <div className="flex flex-col items-stretch">{right}</div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from '../components/Layout'
 import Seo from '../components/Seo'
 import Container from '../components/Container'
 import Hero from '../components/Hero'
+import TwoCols from '../components/TwoCols'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function IndexPage() {
   return (
@@ -15,7 +17,54 @@ export default function IndexPage() {
       />
       <Container>
         <Hero />
+        <TwoCols left={<StoryLeftContent />} right={<StoryRightContent />} />
       </Container>
+      <div className="bg-gray-200">
+        <TwoCols left={<FormLeftContent />} right={<FormRightContent />} />
+      </div>
     </Layout>
   )
 }
+
+const StoryLeftContent = () => (
+  <div className="space-y-5">
+    <h2 className="uppercase text-2xl">Our Story</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempora est
+      earum veritatis libero quaerat, dolore dolores quae omnis, sint ipsam,
+      rerum cumque et neque. Cumque et numquam excepturi aliquid.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nisi
+      architecto laudantium quis officia fugiat voluptatem doloribus voluptates
+      ducimus et velit cupiditate hic recusandae aliquam pariatur, aliquid
+      accusantium illum ipsam.
+    </p>
+  </div>
+)
+
+const StoryRightContent = () => (
+  <StaticImage
+    className="rounded-md"
+    src="../images/sunrise.jpeg"
+    width={400}
+    layout="constrained"
+  />
+)
+
+const FormLeftContent = () => (
+  <div className="space-y-5">
+    <h2 className="uppercase text-2xl">Contact Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum tempora est
+      earum veritatis libero quaerat, dolore dolores quae omnis, sint ipsam,
+      rerum cumque et neque. Cumque et numquam excepturi aliquid.
+    </p>
+  </div>
+)
+
+const FormRightContent = () => (
+  <div className="space-y-5">
+    <h2 className="uppercase text-2xl">Form Goes Here</h2>
+  </div>
+)
