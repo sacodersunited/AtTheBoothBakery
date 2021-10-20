8dfa34b4996252198416ed8298c8ab13b82da2f5
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-10-19 20:22:03 -0500

    more responsive classes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,9 +3,13 @@ import { GatsbyImage } from 'gatsby-plugin-image'
 
 export default function ProductCard(product) {
   return (
-    <div className="w-full relative ">
-      <GatsbyImage image={product.image} alt={product.title} />
-      <div className=" absolute bg-gray-50 text-gray-600 bottom-0 md:bottom-5 p-4">
+    <div className="relative">
+      <GatsbyImage
+        image={product.image}
+        alt={product.title}
+        className="w-full rounded-2xl"
+      />
+      <div className="w-11/12 md:w-full  absolute bg-gray-50 text-gray-600 bottom-0 md:bottom-5 p-4">
         <div className="flex items-center justify-between">
           <h3 className="text-brand-pink uppercase font-bold">
             {product.title}
